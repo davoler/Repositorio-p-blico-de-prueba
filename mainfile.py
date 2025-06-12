c8aa813edb13a5f6ebef7e7bd7e41c24fde886e1
Juan Puerta <juanf.puerta@udea.edu.co>
2025-06-12 14:12:56 -0500

    Initial setup if the obc

--- mainfile.py
+++ mainfile.py
@@ -1,4 +1,32 @@
+"""
+This will be the main file for VSCODE / Github lab
+Here we will be creating a call for spacecraft and their subsystems
+
+"""
+# Libraries
 import numpy as np
+import pandas as pd
+
+# Class Creation
+
+class Satellite:
+    """
+    Represents a class to create a spacecraft
+    """
+    def __init__(self, name, altitude, orbit_type="LEO"):
+        """_summary_
+
+        Args:
+            name (_type_): _Name of the spacecraft_
+            altitude (_type_): _description of the spacecraft_
+            orbit_type (str, optional): _description for the type of orbit_. Defaults to "LEO".
+        """
+        
+        self.name = name
+        self.altitude = altitude  # in kilometers
+        self.orbit_type = orbit_type
+        self.status = "Idle"
+        self.data_collected = 0 # in GB
 
 """This part will be related to the OBC Subsystem"""
 
