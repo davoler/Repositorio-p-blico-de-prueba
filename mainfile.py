3ab2841b31d52a7a083527915ae17bef98bc2972
Juan Puerta <jfpuerta@gmail.com>
2025-06-12 12:28:58 -0500

    Initial normalization function

--- mainfile.py
+++ mainfile.py
@@ -2,6 +2,6 @@ import numpy as np
 
 """This part will be related to the OBC Subsystem"""
 
-Class OBC:
-    def
-    
+def quat_normalize(q):
+    norm = np.linalg.norm(q)
+    return q / norm if norm > 1e-12 else np.array([0.0, 0.0, 0.0, 1.0])
